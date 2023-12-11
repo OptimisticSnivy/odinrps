85b13e4a845cfde1783b91480e431c1eb80a8f36
Anand Mahamuni <m.anand7203@gmail.com>
2023-12-11 19:26:05 +0530

    Base playerChoice using clicks, done!

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function getComputerChoice(){
-    const choice=["rock", "paper", "scissors"];
+    const choice=["Rock","Paper","Scissors"];
     const randomChoice = Math.floor(Math.random()*choice.length);
     return choice[randomChoice];
 }
@@ -7,48 +7,51 @@ function getComputerChoice(){
 function playRound(playerSelection, computerSelection){
     if(playerSelection == computerSelection){
         const res = "Draw!";
-        return res;
+        console.log(res);
     }
-    else if(playerSelection=="rock" && computerSelection=="scissors" || playerSelection=="paper" && computerSelection=="rock" || playerSelection=="scissors" && computerSelection=="paper"){
+    else if(playerSelection=="Rock" && computerSelection=="Scissors" || playerSelection=="Paper" && computerSelection=="Rock" || playerSelection=="Scissors" && computerSelection=="Paper"){
         const res = "Player Wins!";
-        return res;
+        console.log(res);
     }
-    else if(computerSelection=="rock" && playerSelection=="scissors" ||computerSelection=="paper" && playerSelection=="rock" || computerSelection=="scissors" && playerSelection=="paper"){
+    else if(computerSelection=="Rock" && playerSelection=="Scissors" ||computerSelection=="Paper" && playerSelection=="Rock" || computerSelection=="Scissors" && playerSelection=="Paper"){
         const res = "Computer Wins!";
-        return res;
-    }
-    else{
-        const res = "ERR: User Input must ne wrong!";
-        return res;
+        console.log(res);
     }
 }
 
-function game(){
-    let pcount=0;
-    let ccount=0;
-    for(let i=0; i<5; i++){
-        var playerSelection = prompt("Please Enter Your Choice:");
+var buttons = document.querySelectorAll(".btn").length;
 
-        let ps = playerSelection.toLowerCase();
+for (var i = 0; i < buttons ; i++) {
+    const currentbtn = document.querySelectorAll(".btn")[i];
+    currentbtn.addEventListener("click", function() {
+        let result = currentbtn.textContent;
         const cc = getComputerChoice();
-        let playres = playRound(ps,cc);
-        if(playres=="Player Wins!"){
-            pcount=pcount+1;
-            console.log("Player Wins this Round!");
-        }
-        else if(playres=="Computer Wins!"){
-            ccount=ccount+1;
-            console.log("Computer Wins this Round!");
-        }  
-        else{
-            console.log("Draw!");
-        }
-    }
-    console.log("Score:-");
-    console.log("Player:" + pcount);
-    console.log("Computer:" + ccount);
+        console.log(result+" "+cc);
+        playRound(result,cc);
+    });
 }
 
-game();
+// function game(playerChoice){
+//     let pcount=0;
+//     let ccount=0;
+//     for(let i=0; i<5; i++){
+//         let ps = playerChoice;
+//         const cc = getComputerChoice();
+//         let playres = playRound(ps,cc);
+//         if(playres=="Player Wins!"){
+//             pcount=pcount+1;
+//             console.log("Player Wins this Round!");
+//         }
+//         else if(playres=="Computer Wins!"){
+//             ccount=ccount+1;
+//             console.log("Computer Wins this Round!");
+//         }  
+//         else{
+//             console.log("Draw!");
+//         }
+//     }
+//     console.log("Score:-");
+//     console.log("Player:" + pcount);
+//     console.log("Computer:" + ccount);
+// }
 
-//Implementing UI here
